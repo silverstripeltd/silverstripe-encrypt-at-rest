1b4f1c9ca6d50d0b74d74ceb86c1d1402c485252
Simon Erkelens <simonerkelens@silverstripe.com>
2016-03-09 14:33:55 +1300

    Casting to integer. Codeformatting.

--- code/fieldtypes/EncryptedInt.php
+++ code/fieldtypes/EncryptedInt.php
@@ -13,21 +13,26 @@ use \DB,
  * This class wraps around an Int, storing the value in the database as an encrypted string in a varchar field, but
  * returning it to SilverStripe as a decrypted Int object.
  */
-class EncryptedInt extends Int {
-    public function getValue() {
+class EncryptedInt extends Int
+{
+
+
+    public function getValue()
+    {
         $value = $this->value;
         $decryptor = new AtRestCryptoService();
-        return $decryptor->decrypt($value);
+        return (int)$decryptor->decrypt($value);
     }
 
-    public function requireField() {
+    public function requireField()
+    {
         $values = array(
-            'type' => 'varchar',
+            'type'  => 'varchar',
             'parts' => array(
-                'datatype' => 'varchar',
-                'precision' => 255,
-                'null' => 'not null',
-                'default' => $this->defaultVal,
+                'datatype'   => 'varchar',
+                'precision'  => 255,
+                'null'       => 'not null',
+                'default'    => $this->defaultVal,
                 'arrayValue' => $this->arrayValue
             )
         );
@@ -35,7 +40,8 @@ class EncryptedInt extends Int {
         DB::require_field($this->tableName, $this->name, $values);
     }
 
-    public function prepValueForDB($value) {
+    public function prepValueForDB($value)
+    {
         $value = parent::prepValueForDB($value);
         $encryptor = new AtRestCryptoService();
         $ciphertext = $encryptor->encrypt($value);
--- code/fieldtypes/EncryptedVarchar.php
+++ code/fieldtypes/EncryptedVarchar.php
@@ -12,28 +12,32 @@ use \DB,
  * This class wraps around a Varchar, storing the value in the database as an encrypted string in a larger varchar
  * field, and returning the decrypted value.
  */
-class EncryptedVarchar extends Varchar {
-    public function getValue() {
+class EncryptedVarchar extends Varchar
+{
+    public function getValue()
+    {
         return Injector::inst()->get('EncryptAtRest\AtRestCryptoService')->decrypt($this->value);
     }
 
-    public function requireField() {
+    public function requireField()
+    {
         $service = Injector::inst()->get('EncryptAtRest\AtRestCryptoService');
         $values = array(
-            'type' => 'varchar',
+            'type'  => 'varchar',
             'parts' => array(
-                'datatype' => 'varchar',
-                'precision' => $service->calculateRequiredFieldSize($this->size),
+                'datatype'      => 'varchar',
+                'precision'     => $service->calculateRequiredFieldSize($this->size),
                 'character set' => 'utf8',
-                'collate' => 'utf8_general_ci',
-                'arrayValue' => $this->arrayValue
+                'collate'       => 'utf8_general_ci',
+                'arrayValue'    => $this->arrayValue
             )
         );
 
         DB::require_field($this->tableName, $this->name, $values);
     }
 
-    public function prepValueForDB($value) {
+    public function prepValueForDB($value)
+    {
         $value = parent::prepValueForDB($value);
         $encryptor = new AtRestCryptoService();
         $ciphertext = $encryptor->encrypt($value);
