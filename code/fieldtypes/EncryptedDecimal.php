f9f358185de7e408f711ed65018a57dee8ba0643
Steve <steve@silverstripe.com>
2016-05-03 12:58:28 +1200

    Update EncryptedDecimal.php

--- code/fieldtypes/EncryptedDecimal.php
+++ code/fieldtypes/EncryptedDecimal.php
@@ -34,7 +34,7 @@ class EncryptedDecimal extends Decimal
     public function getDecryptedValue($value)
     {
         // Test if we're actually an encrypted value;
-        if (ctype_xdigit($value)) {
+        if (ctype_xdigit($value) && strlen($value) > 130) {
             return $this->service->decrypt($value);
         }
         return (float)$value;
